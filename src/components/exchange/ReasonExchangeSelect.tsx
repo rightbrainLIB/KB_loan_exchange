f5badee59e448d2dc4e07fed9473853b8cebc3a7
RByunsu <yss@rightbrain.co.kr>
2024-01-24 12:04:28 +0900

    [수정, 추가] 환전사유 선택 / 바텀시트일 경우 아닐 경우 구분하여 제작(미확정)

--- src/components/exchange/ReasonExchangeSelect.tsx
+++ src/components/exchange/ReasonExchangeSelect.tsx
@@ -1,17 +1,17 @@
 /**
  * Step 05. 출금정보입력(Progress bar 5/7)
- * 환전사유 선택 / 바텀시트일 경우
+ * 환전사유 선택 / 바텀시트 아닌 경우
  * 환전 사유를 알려주세요
  */
 import KBTalk from "@components/box/KBTalk.tsx";
 import BotProfile from "@components/imgs/BotProfile.tsx";
 import MotionList from "@components/motion/MotionList.tsx";
-import img from "@imgs/exchange/ReasonExchange.png";
+import img from "@imgs/exchange/ReasonExchangeSelect.png";
 import SelectableBtn from "@src/components/buttons/SelectableBtn";
 import SelectableListWrap from "@src/components/list/SelectableListWrap";
 import { FC } from "react";
 
-const ReasonExchange: FC = () => {
+const ReasonExchangeSelect: FC = () => {
   return (
     <>
     <MotionList>
@@ -20,8 +20,23 @@ const ReasonExchange: FC = () => {
 				<img src={img} /> 
 				<SelectableListWrap>
 					<li>
-            <SelectableBtn bgBtn>
-							환전 사유 선택
+            <SelectableBtn>
+							관광 등 일반해외여행경비
+						</SelectableBtn>
+          </li>
+          <li>
+            <SelectableBtn>
+              보유목적
+						</SelectableBtn>
+          </li>
+          <li>
+            <SelectableBtn>
+              유학경비
+						</SelectableBtn>
+          </li>
+					<li>
+            <SelectableBtn>
+              해외체재비
 						</SelectableBtn>
           </li>
 				</SelectableListWrap>
@@ -31,4 +46,4 @@ const ReasonExchange: FC = () => {
   );
 };
 
-export default ReasonExchange;
+export default ReasonExchangeSelect;

ff34b2c3e2ae7270d80cd85f223854a7a4652bac
thl <thl@rightbrain.co.kr>
2024-01-25 10:37:13 +0900

    router 수정 및 container bottom 사이즈 모바일/PC 구분

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -10,48 +10,51 @@ import ExchangeChatBot from "@pages/exchange/ChatBot.tsx";
 import ExchangeMain from "@pages/exchange/Main.tsx";
 import { createBrowserRouter } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/KB_loan_exchange",
-    element: <ExchangeMain />
-  },
-  {
-    path: "/ExchangeChatBot",
-    element: <ExchangeChatBot />
-  },
-  // 대출
-  {
-    path: "/Loan",
-    element: <LoanCreditLimitProduct />
-  },
-  {
-    path: "/LoanImportCheck",
-    element: <LoanImportCheck />
-  },
-  {
-    path: "/LoanSuitableConfirm",
-    element: <LoanSuitableConfirm />
-  },
-  {
-    path: "/LoanFacePop",
-    element: <LoanFacePop />
-  },
-  {
-    path: "/LoanChat",
-    element: <LoanChat />
-  },
-  {
-    path: "/LoanAgreeCheck",
-    element: <LoanAgreeCheck />
-  },
-  {
-    path: "/LoanTelecomSelectPop",
-    element: <LoanTelecomSelectPop />
-  },
-  {
-    path: "/LoanTelecomInputPop",
-    element: <LoanTelecomInputPop />
-  }
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <ExchangeMain />
+    },
+    {
+      path: "/ExchangeChatBot",
+      element: <ExchangeChatBot />
+    },
+    // 대출
+    {
+      path: "/Loan",
+      element: <LoanCreditLimitProduct />
+    },
+    {
+      path: "/LoanImportCheck",
+      element: <LoanImportCheck />
+    },
+    {
+      path: "/LoanSuitableConfirm",
+      element: <LoanSuitableConfirm />
+    },
+    {
+      path: "/LoanFacePop",
+      element: <LoanFacePop />
+    },
+    {
+      path: "/LoanChat",
+      element: <LoanChat />
+    },
+    {
+      path: "/LoanAgreeCheck",
+      element: <LoanAgreeCheck />
+    },
+    {
+      path: "/LoanTelecomSelectPop",
+      element: <LoanTelecomSelectPop />
+    },
+    {
+      path: "/LoanTelecomInputPop",
+      element: <LoanTelecomInputPop />
+    }
+  ],
+  { basename: "/KB_loan_exchange" }
+);
 
 export default router;
